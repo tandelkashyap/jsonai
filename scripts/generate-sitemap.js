7c86bc2b49355f5f2ea56fbe8e65d714dff4ffa5
tandelkashyap <tandelkashyap33@gmail.com>
2026-08-17 13:38:06 +0530

    Resolving 3XX redirect issue

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -30,7 +30,7 @@ const routes = [
 const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${routes.map(r => `  <url>
-    <loc>${hostname}${r.loc}</loc>
+    <loc>${hostname}${r.loc}${r.loc.endsWith('/') ? '' : '/'}</loc>
     <lastmod>${today}</lastmod>
     <changefreq>${r.changefreq}</changefreq>
     <priority>${r.priority}</priority>
